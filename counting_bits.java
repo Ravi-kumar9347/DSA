bdf559b45a2a5c48e9a36c08812059f977cca754
Ravi kumar <126487842+Ravi-kumar9347@users.noreply.github.com>
2023-09-01 18:30:39 +0530

    DSA

---
+++ counting_bits.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] countBits(int n) {
+        //O(N)
+        int[] result = new int[n+1];
+
+        for(int i = 1; i <= n; i++){
+            result[i] = result[i/2] + (i%2);
+        }
+        return result;
+    }
+}
