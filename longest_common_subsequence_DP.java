6ea5d512b4a6996f0e0ba8fda89405f1c154315e
Ravi kumar <126487842+Ravi-kumar9347@users.noreply.github.com>
2023-09-05 16:28:40 +0530

    DP

---
+++ longest_common_subsequence_DP.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestCommonSubsequence(String s, String t) {
+        //O(N*M)
+        //S(M)
+        int n = s.length();
+        int m = t.length();
+
+        int[]prev = new int[m+1];
+
+        for(int i = n - 1; i >= 0; i--){
+            int[]cur = new int[m+1];
+            for(int j = m - 1; j >= 0; j--){
+                int take = 0;
+                if(s.charAt(i) == t.charAt(j)) take = 1 + prev[j+1];
+                cur[j] = Math.max(cur[j+1], Math.max(prev[j], take));
+            }
+            prev = cur;
+        }
+
+        return prev[0];
+    }
+}
