6352c9fb428572e319828910ab36ccf5d4d003af
Ravi kumar <126487842+Ravi-kumar9347@users.noreply.github.com>
2023-09-05 16:21:25 +0530

    DSA

---
+++ print_adjacency_list.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<Integer>> printGraph(int V, int edges[][]) {
+        //O(V+E)
+        List<List<Integer>> adj = new ArrayList<>();
+        
+        for(int i = 0; i < V; i++) adj.add(new ArrayList<>());
+        
+        for(int[] edge : edges){
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+        
+        return adj;
+    }
+}
