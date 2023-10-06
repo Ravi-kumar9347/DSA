dd9f99ff5ba3d68475fe0e666a3188a56b7d8ba8
Ravi kumar <126487842+Ravi-kumar9347@users.noreply.github.com>
2023-10-06 23:56:54 +0530

    Update integer_break.java

--- integer_break.java
+++ integer_break.java
@@ -1,19 +1,43 @@
 class Solution {
     public int integerBreak(int n) {
-        //O(N)
-        //S(1)
+        
         if (n <= 3) {
             return n - 1; // For n <= 3, maximum product is obtained by breaking it into (n - 1).
         }
 
-        int[] dp = new int[n + 1];
-        dp[2] = 2;
-        dp[3] = 3;
+        //O(N)
+        //S(1)
 
-        for (int i = 4; i <= n; i++) {
-            dp[i] = Math.max(2 * dp[i - 2], 3 * dp[i - 3]);
+        // int[] dp = new int[n + 1];
+        // dp[2] = 2;
+        // dp[3] = 3;
+
+        // for (int i = 4; i <= n; i++) {
+        //     dp[i] = Math.max(2 * dp[i - 2], 3 * dp[i - 3]);
+        // }
+
+        // return dp[n];
+
+        //O(1)
+        //S(1)
+        // int result = 1;
+        // while (n > 4) {
+        //     result *= 3;
+        //     n -= 3;
+        // }
+        // result *= n;
+        // return result;
+
+        //O(1)
+        //S(1)
+        int quotient = n / 3;
+        int remainder = n % 3;
+        if (remainder == 0) {
+            return (int) Math.pow(3, quotient);
+        } else if (remainder == 1) {
+            return (int) (Math.pow(3, quotient - 1) * 4);
+        } else {
+            return (int) (Math.pow(3, quotient) * 2);
         }
-
-        return dp[n];
     }
 }
