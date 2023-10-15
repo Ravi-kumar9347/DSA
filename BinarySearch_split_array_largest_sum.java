1fb3575851800a6430c002db692b074649b101eb
Ravi-kumar9347 <raviroy4839@gmail.com>
2023-10-15 13:46:33 +0530

    main

---
+++ BinarySearch_split_array_largest_sum.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    private static int split(int largestSum, int[] nums){
+        int count = 0, currentSum = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(currentSum + nums[i] <= largestSum){
+                currentSum += nums[i];
+            }
+            else{
+                count++;
+                currentSum = nums[i];
+            }
+        }
+        count++;
+        return count;
+    }
+
+    public int splitArray(int[] nums, int k) {
+        //O(N * log(sum(nums) - max(nums)))
+        //S(1)
+        int low = -1, high = 0;
+        
+        for(int num : nums){
+            low = Math.max(low, num);
+            high += num;
+        }
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(split(mid, nums) <= k){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+}
