41b53871deffbea85fcea480b23830be0cf5b313
Ravi-kumar9347 <raviroy4839@gmail.com>
2023-10-21 13:49:31 +0530

    main

---
+++ Binarysearch_median_of_two_sorted_arrays.java
@@ -0,0 +1,39 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        //O(logN1) -> n1 < n2
+        //S(1)
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        if(n1 > n2) return findMedianSortedArrays(nums2, nums1);
+
+        int left = (n1 + n2 + 1) / 2;
+        int low = 0, high = n1;
+        while(low <= high){
+            int mid1 = (low + high) / 2;
+            int mid2 = left - mid1;
+
+            int l1 = (mid1 - 1 >= 0) ? nums1[mid1 - 1] : Integer.MIN_VALUE;
+            int l2 = (mid2 - 1 >= 0) ? nums2[mid2 - 1] : Integer.MIN_VALUE;
+            int r1 = (mid1 < n1) ? nums1[mid1] : Integer.MAX_VALUE;
+            int r2 = (mid2 < n2) ? nums2[mid2] : Integer.MAX_VALUE;
+
+            if(l1 <= r2 && l2 <= r1){
+                if((n1 + n2) % 2 == 0){
+                    return (double)(Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                }
+                else{
+                    return (Math.max(l1, l2));
+                }
+            }
+            
+            if(l1 > r2){
+                high = mid1 - 1;
+            }
+            else if(l2 > r1){
+                low = mid1 + 1;
+            }
+        }
+
+        return 0.0;
+    }
+}
