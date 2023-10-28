6960650d7fc833b09af3f5edb902ac0d57d83ce8
Ravi-kumar9347 <raviroy4839@gmail.com>
2023-10-28 16:15:17 +0530

    main

---
+++ gfg_potd_bleak_numbers.java
@@ -0,0 +1,23 @@
+
+class Solution
+{
+    public int is_bleak(int n)
+    {
+        //O(logN * logN)
+        for(int i=1;i<=Math.min(31,n);i++)
+        {
+            int number=n-i,cc=0;
+            while(number>0)
+            {
+                cc+=number&1;
+                number>>=1;
+            }
+            if(cc==i)
+            {
+                return 0;
+            }
+        }
+        return 1;
+    }
+    
+}
